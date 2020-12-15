2d1c7feaa7ee4b2ff153a3aeb873adfbc0627a78
Mohamed Hamed <mo7amed7amed2030@outlook.com>
2020-12-15 11:14:34 +0200

    add team-city-couch-country-stadium controller

---
+++ KoraOnlineAdmin/Controllers/StadiaController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using KoraOnlineAdmin.Models;
+
+namespace KoraOnlineAdmin.Controllers
+{
+    public class StadiaController : Controller
+    {
+        private readonly KoraOnline _context;
+
+        public StadiaController(KoraOnline context)
+        {
+            _context = context;
+        }
+
+        // GET: Stadia
+        public async Task<IActionResult> Index()
+        {
+            var koraOnline = _context.Stadia.Include(s => s.City);
+            return View(await koraOnline.ToListAsync());
+        }
+
+        // GET: Stadia/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var stadium = await _context.Stadia
+                .Include(s => s.City)
+                .FirstOrDefaultAsync(m => m.StadiumId == id);
+            if (stadium == null)
+            {
+                return NotFound();
+            }
+
+            return View(stadium);
+        }
+
+        // GET: Stadia/Create
+        public IActionResult Create()
+        {
+            ViewData["CityId"] = new SelectList(_context.Cities, "CityId", "CityId");
+            return View();
+        }
+
+        // POST: Stadia/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("StadiumId,StadiumName,SatdiumCapacity,CityId")] Stadium stadium)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(stadium);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CityId"] = new SelectList(_context.Cities, "CityId", "CityId", stadium.CityId);
+            return View(stadium);
+        }
+
+        // GET: Stadia/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var stadium = await _context.Stadia.FindAsync(id);
+            if (stadium == null)
+            {
+                return NotFound();
+            }
+            ViewData["CityId"] = new SelectList(_context.Cities, "CityId", "CityId", stadium.CityId);
+            return View(stadium);
+        }
+
+        // POST: Stadia/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("StadiumId,StadiumName,SatdiumCapacity,CityId")] Stadium stadium)
+        {
+            if (id != stadium.StadiumId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(stadium);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!StadiumExists(stadium.StadiumId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CityId"] = new SelectList(_context.Cities, "CityId", "CityId", stadium.CityId);
+            return View(stadium);
+        }
+
+        // GET: Stadia/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var stadium = await _context.Stadia
+                .Include(s => s.City)
+                .FirstOrDefaultAsync(m => m.StadiumId == id);
+            if (stadium == null)
+            {
+                return NotFound();
+            }
+
+            return View(stadium);
+        }
+
+        // POST: Stadia/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var stadium = await _context.Stadia.FindAsync(id);
+            _context.Stadia.Remove(stadium);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool StadiumExists(int id)
+        {
+            return _context.Stadia.Any(e => e.StadiumId == id);
+        }
+    }
+}
---
+++ KoraOnlineAdmin/Controllers/TeamsController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using KoraOnlineAdmin.Models;
+
+namespace KoraOnlineAdmin.Controllers
+{
+    public class TeamsController : Controller
+    {
+        private readonly KoraOnline _context;
+
+        public TeamsController(KoraOnline context)
+        {
+            _context = context;
+        }
+
+        // GET: Teams
+        public async Task<IActionResult> Index()
+        {
+            var koraOnline = _context.Teams.Include(t => t.Coach).Include(t => t.League);
+            return View(await koraOnline.ToListAsync());
+        }
+
+        // GET: Teams/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var team = await _context.Teams
+                .Include(t => t.Coach)
+                .Include(t => t.League)
+                .FirstOrDefaultAsync(m => m.TeamId == id);
+            if (team == null)
+            {
+                return NotFound();
+            }
+
+            return View(team);
+        }
+
+        // GET: Teams/Create
+        public IActionResult Create()
+        {
+            ViewData["CoachId"] = new SelectList(_context.Coaches, "CoachId", "CoachId");
+            ViewData["LeagueId"] = new SelectList(_context.Leagues, "LeagueId", "LeagueId");
+            return View();
+        }
+
+        // POST: Teams/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("TeamId,TeamName,TeamMatchPlayed,TeamWon,TeamDraw,TeamLost,TeamGoalFor,TeamGoalAg,TeamGoalOn,LeagueId,CoachId,TeamImage")] Team team)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(team);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CoachId"] = new SelectList(_context.Coaches, "CoachId", "CoachId", team.CoachId);
+            ViewData["LeagueId"] = new SelectList(_context.Leagues, "LeagueId", "LeagueId", team.LeagueId);
+            return View(team);
+        }
+
+        // GET: Teams/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var team = await _context.Teams.FindAsync(id);
+            if (team == null)
+            {
+                return NotFound();
+            }
+            ViewData["CoachId"] = new SelectList(_context.Coaches, "CoachId", "CoachId", team.CoachId);
+            ViewData["LeagueId"] = new SelectList(_context.Leagues, "LeagueId", "LeagueId", team.LeagueId);
+            return View(team);
+        }
+
+        // POST: Teams/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("TeamId,TeamName,TeamMatchPlayed,TeamWon,TeamDraw,TeamLost,TeamGoalFor,TeamGoalAg,TeamGoalOn,LeagueId,CoachId,TeamImage")] Team team)
+        {
+            if (id != team.TeamId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(team);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TeamExists(team.TeamId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CoachId"] = new SelectList(_context.Coaches, "CoachId", "CoachId", team.CoachId);
+            ViewData["LeagueId"] = new SelectList(_context.Leagues, "LeagueId", "LeagueId", team.LeagueId);
+            return View(team);
+        }
+
+        // GET: Teams/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var team = await _context.Teams
+                .Include(t => t.Coach)
+                .Include(t => t.League)
+                .FirstOrDefaultAsync(m => m.TeamId == id);
+            if (team == null)
+            {
+                return NotFound();
+            }
+
+            return View(team);
+        }
+
+        // POST: Teams/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var team = await _context.Teams.FindAsync(id);
+            _context.Teams.Remove(team);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TeamExists(int id)
+        {
+            return _context.Teams.Any(e => e.TeamId == id);
+        }
+    }
+}
